6e635d3522eff893a69918bd4a247232b2b794b8
Alaa Refaat <75855409+nad1720@users.noreply.github.com>
2023-04-18 06:09:31 +0200

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() {
-  runApp(BirthdayCardApp());
-}
+void main() => runApp(MyApp());
 
-class BirthdayCardApp extends StatelessWidget {
-  const BirthdayCardApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Birthday Card',
+      title: 'Business Card',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Birthday Card'),
+          title: Text('Business Card'),
         ),
+        backgroundColor: Colors.teal.shade900,
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/Boo.jpg'),
+              ),
+              SizedBox(
+                height: 10,
+              ),
               Text(
-                'Happy Birthday !!',
-                style: TextStyle(
-                  fontSize: 32.0,
+                'Alaa Refaat',
+                style: GoogleFonts.pacifico(
+                  color: Colors.white,
+                  fontSize: 30,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              SizedBox(height: 20.0),
-              Image.asset(
-                'images/birth.png',
-                height: 300.0,
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                'Flutter Developer',
+                style: GoogleFonts.sourceSansPro(
+                  fontSize: 20,
+                  color: Colors.white,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+20 128 744 7514',
+                    style: GoogleFonts.sourceSansPro(
+                      color: Colors.teal.shade900,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'na1720@gmail.com',
+                    style: GoogleFonts.sourceSansPro(
+                      color: Colors.teal.shade900,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
