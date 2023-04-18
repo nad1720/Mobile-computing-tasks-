a1e4f95df06a58e703a25b2710138e6a808af07b
Alaa Refaat <75855409+nad1720@users.noreply.github.com>
2023-04-19 01:59:21 +0200

    Delete android directory

--- android/app/src/main/kotlin/com/example/birthdaycard/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.birthdaycard
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/example/bussinesscard/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.bussinesscard
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
