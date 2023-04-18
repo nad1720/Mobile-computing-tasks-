9cb5c64eb13ebc7650ff85787d090b4f44b75e45
Alaa Refaat <75855409+nad1720@users.noreply.github.com>
2023-04-18 22:13:11 +0200

    mobile Asignments
    this for all mobile computing Assignments for the last semester

---
+++ Lab5 assignment/bussinesscard/android/app/src/main/kotlin/com/example/bussinesscard/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bussinesscard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lab4 assignment/birthdaycard/android/app/src/main/kotlin/com/example/birthdaycard/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.birthdaycard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
