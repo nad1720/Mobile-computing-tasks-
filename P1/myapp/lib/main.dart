3a9151f43484b2a7a781b1d4962fe22e690db152
Alaa Refaat <75855409+nad1720@users.noreply.github.com>
2023-05-26 17:33:18 +0300

    Update main.dart

--- P1/myapp/lib/main.dart
+++ P1/myapp/lib/main.dart
@@ -1,19 +1,23 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:sensors_plus/sensors_plus.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
-enum UserActivity { unknown, walking, running }
+enum UserActivity { unknown, walking, running, jugging }
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'My App',
       home: MyHomePage(title: 'My App Home Page'),
     );
@@ -21,72 +25,171 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
+StreamController<UserActivity> _userActivityStream = StreamController<UserActivity>();
 
 class _MyHomePageState extends State<MyHomePage> {
-  MapController _mapController = MapController();
-  GeolocatorPlatform _geolocator = GeolocatorPlatform.instance;
+  final MapController _mapController = MapController();
+  final GeolocatorPlatform _geolocator = GeolocatorPlatform.instance;
   UserActivity _userActivity = UserActivity.unknown;
-  List<LatLng> _points = [];
-  List<Marker> _markers = [];
-  List<LatLng> _route = [];
-  Polyline _polyline = Polyline(
+  final List<Marker> _markers = [];
+  final List<LatLng> __route = [];
+  //final url = 'path/to/local/tiles/{z}/{x}/{y}.png';
+  //final file = DefaultCacheManager().getSingleFile('assets/');
+  late Polyline _polyline = Polyline(
     points: [],
     color: Colors.red,
     strokeWidth: 4.0,
   );
-
   @override
   void initState() {
     super.initState();
+    _initPlatformState();
+    _initSensors();
+    _getCurrentLocation();
+    _listenToLocationChanges();
+  }
+  Future<void> _listenToLocationChanges() async {
+    Geolocator.getPositionStream(
+      desiredAccuracy: LocationAccuracy.high,
+      distanceFilter: 10,
+    ).listen((Position position) {
+      _onLocationUpdate(position);
+    });
+  }
 
-    // Start listening to location updates
-    _geolocator.getPositionStream().listen((position) {
-      // Update the map view with the user's current location
-      _mapController.move(LatLng(position.latitude, position.longitude), 15);
+  Future<void> _initPlatformState() async {
+    await _geolocator.isLocationServiceEnabled();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    Geolocator.getPositionStream(
+      desiredAccuracy: LocationAccuracy.high,
+      distanceFilter: 10,
+    ).listen((Position position) {
+      _onLocationUpdate(position);
+    });
+  }
+
+  void _onLocationUpdate(Position position) {
+    LatLng latLng = LatLng(position.latitude, position.longitude);
+    double speed = position.speed;
+
+    // تحديد نشاط المستخدم بناءً على السرعة الحالية
+    if (speed > 2.0) {
       setState(() {
-        _markers.clear();
-        _markers.add(Marker(
-          width: 80.0,
-          height: 80.0,
-          point: LatLng(position.latitude, position.longitude),
-          builder: (ctx) => Container(
-            child: Icon(Icons.person, color: Colors.blue),
-          ),
-        ));
-        _points.add(LatLng(position.latitude, position.longitude));
-        _route = List.from(_points);
-        _polyline = Polyline(
-          points: _route,
-          color: Colors.red,
-          strokeWidth: 4.0,
-        );
+        _userActivity = UserActivity.running;
       });
+    } else if (speed > 1.0) {
+      setState(() {
+        _userActivity = UserActivity.jugging;
+      });
+    } else if (speed > 0.5) {
+      setState(() {
+        _userActivity = UserActivity.walking;
+      });
+    } else {
+      setState(() {
+        _userActivity = UserActivity.unknown;
+      });
+    }
+
+    // إضافة قيمة _userActivity إلى _userActivityStream
+    _userActivityStream.add(_userActivity);
+
+    // إعادة بناء واجهة المستخدم
+    setState(() {
+      _markers.add(Marker(
+        width: 80.0,
+        height: 80.0,
+        point: latLng,
+        builder: (ctx) => const Icon(Icons.location_on),
+      ));
+      __route.add(latLng);
+      _polyline = Polyline(
+        points: __route,
+        color: Colors.red,
+        strokeWidth: 4.0,
+      );
     });
 
-    // Start listening to accelerometer updates
+    // تحديث مركز الخريطة
+    _mapController.move(latLng, 16);
+  }
+  void _initSensors() {
     accelerometerEvents.listen((AccelerometerEvent event) {
-      // Calculate the magnitude of the acceleration vector
-      double acceleration =
-          event.x * event.x + event.y * event.y + event.z * event.z;
+      double x = event.x;
+      double y = event.y;
+      double z = event.z;
 
-      // Determine whether the user is walking or running based on the magnitude of the acceleration vector
-      if (acceleration > 25.0) {
-        setState(() {
-          _userActivity = UserActivity.running;
-        });
+      // تحديد حركة المستخدم بناءً على قراءات مستشعر accelerometer
+      if (x.abs() > y.abs()) {
+        if (x > 0) {
+          _userActivityStream.add(UserActivity.running);
+        } else {
+          _userActivityStream.add(UserActivity.jugging);
+        }
       } else {
-        setState(() {
-          _userActivity = UserActivity.walking;
-        });
+        if (y > 0) {
+          _userActivityStream.add(UserActivity.walking);
+        } else {
+          _userActivityStream.add(UserActivity.unknown);
+        }
       }
     });
+  }
+  @override
+  void dispose() {
+    _userActivityStream.close();
+    super.dispose();
+  }
+
+  Widget _buildMap() {
+    return StreamBuilder<UserActivity>(
+      stream: _userActivityStream.stream,
+      initialData: UserActivity.unknown,
+      builder: (BuildContext context, AsyncSnapshot<UserActivity> snapshot) {
+        if (snapshot.hasData) {
+          _userActivity = snapshot.data!;
+        }
+        return FlutterMap(
+          mapController: _mapController,
+          options: MapOptions(
+            center: LatLng(37.7749, -122.4194),
+            zoom: 13.0,
+          ),
+          layers: [
+            TileLayerOptions(
+              urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+              subdomains: ['a', 'b', 'c'],
+            ),
+            PolylineLayerOptions(
+              polylines: [_polyline],
+            ),
+            MarkerLayerOptions(
+              markers: [
+                Marker(
+                  width: 80.0,
+                  height: 80.0,
+                  point: __route.isNotEmpty ? __route.last : LatLng(0, 0),
+                  builder: (ctx) => const Icon(
+                    Icons.person_pin_circle_outlined,
+                    color: Colors.blue,
+                    size: 50.0,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -95,54 +198,37 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: FlutterMap(
-        mapController: _mapController,
-        options: MapOptions(
-          center: LatLng(29.309, 30.841),
-          zoom: 13.0,
-        ),
-        layers: [
-          TileLayerOptions(
-            urlTemplate: "assets/{z}/{x}/{y}.png",
-            tileProvider: const AssetTileProvider(),
-            maxZoom: 18.0,
-          ),
-          PolylineLayerOptions(
-            polylines: [_polyline],
-          ),
-          MarkerLayerOptions(markers: _markers),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Center the map view on the user's current location
-          _geolocator.getCurrentPosition().then((position) {
-            _mapController.move(
-                LatLng(position.latitude, position.longitude), 15);
-          });
-        },
-        child: Icon(Icons.my_location),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: Container(
-          height: 60.0,
-          child: Center(
+      body: Stack(
+        children: [
+          _buildMap(),
+          Positioned(
+            bottom:20,
+            left: 20,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
-                  'User activity:',
-                ),
-                Text(
                   _userActivity.toString(),
-                  style: Theme.of(context).textTheme.headline4,
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline4
+                      ?.copyWith(backgroundColor: Colors.white),
                 ),
               ],
             ),
           ),
-        ),
+        ],
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          Position position = await _geolocator.getCurrentPosition(
+            desiredAccuracy: LocationAccuracy.high,
+          );
+          _onLocationUpdate(position);
+        },
+        tooltip: 'Get Location',
+        child: const Icon(Icons.my_location),
+      ),
     );
   }
 }
